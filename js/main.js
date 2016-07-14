ed2b32af8930d0dfa2e30257f1741aef9a6b5eab
konumashuri <re.e.poch89@gmail.com>
2016-07-14 18:45:59 +0900

    commit

--- js/main.js
+++ js/main.js
@@ -1,96 +1,18 @@
 /* global chrome */
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
 function getCurrentTabUrl(callback) {
-  // Query filter to be passed to chrome.tabs.query - see
-  // https://developer.chrome.com/extensions/tabs#method-query
   var queryInfo = {
     active: true,
     currentWindow: true
   };
 
   chrome.tabs.query(queryInfo, function(tabs) {
-    // chrome.tabs.query invokes the callback with a list of tabs that match the
-    // query. When the popup is opened, there is certainly a window and at least
-    // one tab, so we can safely assume that |tabs| is a non-empty array.
-    // A window can only have one active tab at a time, so the array consists of
-    // exactly one tab.
     var tab = tabs[0];
-
-    // A tab is a plain object that provides information about the tab.
-    // See https://developer.chrome.com/extensions/tabs#type-Tab
     var url = tab.url;
-
-    // tab.url is only available if the "activeTab" permission is declared.
-    // If you want to see the URL of other tabs (e.g. after removing active:true
-    // from |queryInfo|), then the "tabs" permission is required to see their
-    // "url" properties.
     console.assert(typeof url === 'string', 'tab.url should be a string');
 
     callback(url);
   });
-
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
-
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-// function getImageUrl(searchTerm, callback, errorCallback) {
-//   // Google image search - 100 searches per day.
-//   // https://developers.google.com/image-search/
-//   var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-//     '?v=1.0&q=' + encodeURIComponent(searchTerm);
-//   var x = new XMLHttpRequest();
-//   x.open('GET', searchUrl);
-//   // The Google image search API responds with JSON, so let Chrome parse it.
-//   x.responseType = 'json';
-//   x.onload = function() {
-//     // Parse and process the response from Google Image Search.
-//     var response = x.response;
-//     if (!response || !response.responseData || !response.responseData.results ||
-//         response.responseData.results.length === 0) {
-//       errorCallback('No response from Google Image search!');
-//       return;
-//     }
-//     var firstResult = response.responseData.results[0];
-//     // Take the thumbnail instead of the full image to get an approximately
-//     // consistent image size.
-//     var imageUrl = firstResult.tbUrl;
-//     var width = parseInt(firstResult.tbWidth);
-//     var height = parseInt(firstResult.tbHeight);
-//     console.assert(
-//         typeof imageUrl === 'string' && !isNaN(width) && !isNaN(height),
-//         'Unexpected respose from the Google Image Search API!');
-//     callback(imageUrl, width, height);
-//   };
-//   x.onerror = function() {
-//     errorCallback('Network error.');
-//   };
-//   x.send();
-// }
-
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
 }
 
 function initData() {
@@ -109,27 +31,74 @@ function initData() {
   });
 }
 
-function clearData() {
-  chrome.storage.local.clear();
-}
+var renderStatus = function(statusText, type){
+  var html = '<div class="alert alert-' + type + '" role="alert">' + statusText + '</div>';
+  document.getElementById('status').innerHTML = html;
+};
 
-var clearButton = document.getElementById('clearButton');
-clearButton.addEventListener('click', function() {
+var renderUrls = function(url_hash, date){
+  console.log('rendering urls....');
+  // render UNREAD list
+  var unreadHtml = '<ol>';
+  for(var unreadUrl in url_hash['unread'][date]){
+    unreadHtml += '<li><a href=';
+    unreadHtml += url_hash['unread'][date][unreadUrl];
+    unreadHtml += ' class="unread-url" target="_blank">';
+    unreadHtml += trim(url_hash['unread'][date][unreadUrl]);
+    unreadHtml += '</a></li>';
+  }
+  unreadHtml += '</ol>';
+  document.getElementById('unread-urls').innerHTML = unreadHtml;
+
+  // read list
+  var readHtml = '<ol>';
+  for(var readUrl in url_hash['read'][date]){
+    readHtml += '<li><a href=';
+    readHtml += url_hash['read'][date][readUrl];
+    readHtml += ' class="read-url" target="_blank">';
+    readHtml += trim(url_hash['read'][date][readUrl]);
+    readHtml += '</a></li>';
+  }
+  readHtml += '</ol>';
+  document.getElementById('read-urls').innerHTML = readHtml;
+
+  // add event lister after rendering
+  console.log(className.length);
+  for(var i = 0; i < className.length; i++) {
+    className[i].addEventListener('click', moveUrlToRead, false);
+  }
+};
+
+var trim = function(url) {
+  return url.substring(0, 50) + '...';
+};
+
+var clearData = function() {
+  chrome.storage.local.clear();
+};
+
+var dateParser = function() {
+  var d = new Date();
+  var date = d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear();
+  return date;
+};
+
+document.getElementById('clearButton').addEventListener('click', function() {
+  var date = dateParser();
+
   clearData();
-  renderStatus('Cleared!');
+  initData();
+  renderStatus('Cleared!', 'success');
+  chrome.storage.local.get(function(values){
+    renderUrls(values, date);
+  });
 });
 
-var addButton = document.getElementById('addButton');
-
-addButton.addEventListener('click', function() {
+document.getElementById('addButton').addEventListener('click', function() {
   getCurrentTabUrl(function(url) {
-    // Prepare url hash in local storage
-    initData();
-
     console.log('Detecting if you have registered ' + url);
 
-    var d = new Date();
-    var date = d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear();
+    var date = dateParser();
 
     chrome.storage.local.get(function(values){
 
@@ -138,35 +107,48 @@ addButton.addEventListener('click', function() {
       }
 
       if(values['unread'][date].includes(url)){
-        console.log('Already Saved!');
+        console.log('Already Added!');
+        renderStatus('Already Added!', 'info');
       }else{
         values['unread'][date].push(url);
-        console.log('Saved!');
+        console.log('Added!');
+        renderStatus('Added!', 'success');
       }
-      console.log(values);
 
       chrome.storage.local.set(values);
 
-      renderStatus('OK!');
+      renderUrls(values, date);
     });
-
-
-    // getImageUrl(url, function(imageUrl, width, height) {
-
-    //   renderStatus('Search term: ' + url + '\n' +
-    //       'Google image search result: ' + imageUrl);
-    //   var imageResult = document.getElementById('image-result');
-    //   // Explicitly set the width/height to minimize the number of reflows. For
-    //   // a single image, this does not matter, but if you're going to embed
-    //   // multiple external images in your page, then the absence of width/height
-    //   // attributes causes the popup to resize multiple times.
-    //   imageResult.width = width;
-    //   imageResult.height = height;
-    //   imageResult.src = imageUrl;
-    //   imageResult.hidden = false;
-
-    // }, function(errorMessage) {
-    //   renderStatus('Cannot display image. ' + errorMessage);
-    // });
   });
 });
+
+window.onload = function(){
+  // Prepare url hash in local storage
+  initData();
+  var date = dateParser();
+
+  chrome.storage.local.get(function(values){
+    renderUrls(values, date);
+  });
+};
+
+// move url from unread urls to read urls
+var className = document.getElementsByClassName('unread-url');
+var moveUrlToRead = function(){
+  console.log('move url to read list');
+  var url = this.href;
+  var date = dateParser();
+
+  chrome.storage.local.get(function(values){
+    var index = values['unread'][date].indexOf(url);
+    if (index > -1){
+      values['unread'][date].splice(index, 1);
+      if(typeof(values['read'][date]) === 'undefined'){
+        values['read'][date] = [];
+      }
+      values['read'][date].push(url);
+
+      chrome.storage.local.set(values);
+    }
+  });
+};
